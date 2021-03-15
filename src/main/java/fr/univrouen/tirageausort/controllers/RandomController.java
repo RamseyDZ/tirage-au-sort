1050075f0a21deaf9102a7491e883eac773889eb
Ramzi Bouchair <bouchairramzi@gmail.com>
2021-03-15 01:13:29 +0100

    Feat: adding swagger configuration

--- src/main/java/fr/univrouen/tirageausort/controllers/RandomController.java
+++ src/main/java/fr/univrouen/tirageausort/controllers/RandomController.java
@@ -1,18 +1,19 @@
 package fr.univrouen.tirageausort.controllers;
 
 import fr.univrouen.tirageausort.model.Users;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-@Controller
-public class tirageController {
+@RestController
+@RequestMapping("/api")
+public class RandomController {
 
-    @RequestMapping(value="/getUser")
-    public Users getRandomUser(List<Users> listUsers){
+    @RequestMapping(value = "/getUser")
+    public Users getRandomUser(List<Users> listUsers) {
         var String = "user name";
         // mélanger la liste des utilsateur
         Collections.shuffle(listUsers);
